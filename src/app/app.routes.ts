1427d9dad62597a53a8a4c71c5cf1e7cf9146c5b
Sanae KHAMLICHI <sanaekhamlichi01@gmail.com>
2024-04-26 09:30:08 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'header',
+    loadComponent: () =>
+      import('./header/header.component').then(
+        (c) => c.HeaderComponent,
+      ),
+    title: 'Accueil',
+  },
+];
